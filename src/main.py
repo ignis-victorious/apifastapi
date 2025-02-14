ad1114d964b78fe0e2744ce4c2fb278a6cb0904f
Emagnu <ignisvictorious@gmail.com>
2025-02-14 12:26:07 +0000

    add class, one get and one post with response_model

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 #  ##   ###
 #  Import LIBRARIES
 from fastapi import FastAPI
+from pydantic import BaseModel
+from uuid import UUID, uuid4
 #  Import FILES
 #  ##   ###
 
@@ -8,9 +10,31 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root() -> dict[str, str]:
-    return {"Hello": "World"}
+class Task(BaseModel):
+    id: UUID | None = None
+    title: str
+    description: str | None = None
+    completed: bool = False
+
+
+tasks: list[Task] = []
+
+
+@app.post("/tasks/", response_model=Task)
+def create_task(task: Task) -> Task:
+    task.id = uuid4()
+    tasks.append(task)
+    return task
+
+
+@app.get("/tasks/", response_model=list[Task])
+def read_tasks() -> list[Task]:
+    return tasks
+
+
+# @app.get("/")
+# def read_root() -> dict[str, str]:
+#     return {"Hello": "World"}
 
 
 if __name__ == "__main__":
