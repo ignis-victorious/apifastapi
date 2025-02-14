a559e696247d976d2e2c09fa7efef14d2c591d1b
Emagnu <ignisvictorious@gmail.com>
2025-02-14 15:57:26 +0000

    Correct bugs. All wirking

--- src/main.py
+++ src/main.py
@@ -32,7 +32,7 @@ def read_tasks() -> list[Task]:
     return tasks
 
 
-@app.get("/tasks/{task_id}", response_model=list[Task])
+@app.get("/tasks/{task_id}", response_model=Task)
 def read_task(task_id: UUID) -> Task | HTTPException:
     for task in tasks:
         if task.id == task_id:
@@ -40,7 +40,7 @@ def read_task(task_id: UUID) -> Task | HTTPException:
     raise HTTPException(status_code=404, detail="Task not found")
 
 
-@app.put("/tasks/{task_id}", response_model=list[Task])
+@app.put("/tasks/{task_id}", response_model=Task)
 def update_task(task_id: UUID, task_update: Task) -> Task | HTTPException:
     for idx, task in enumerate(tasks):
         if task.id == task_id:
@@ -58,12 +58,11 @@ def update_task(task_id: UUID, task_update: Task) -> Task | HTTPException:
     raise HTTPException(status_code=404, detail="Task not found")
 
 
-@app.delete("/tasks/{task_id}", response_model=list[Task])
+@app.delete("/tasks/{task_id}", response_model=Task)
 def delete_task(task_id: UUID) -> Task | HTTPException:
     for idx, task in enumerate(tasks):
         if task.id == task_id:
             tasks.pop(idx)
-            return task
     raise HTTPException(status_code=404, detail="Task not found")
 
 
@@ -77,3 +76,30 @@ if __name__ == "__main__":
 
     #  Import FILES LIBRARIES
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+# {
+#   "title": "Task 1",
+#   "description": "This the description of Task 1",
+#   "completed": false
+# }
+# {
+#   "title": "Task 2",
+#   "description": "This the description of Task 2",
+#   "completed": true
+# }
+# {
+#   "title": "Task 3",
+#   "description": "Hello, this the description of Task 3",
+#   "completed": false
+# }
+#  {
+#   "title": "Task 4",
+#   "description": "YES, this the description of not Task 3 but Task 4 ",
+#   "completed": false
+# }
+# {
+#     "title": "Task 5",
+#     "description": "NEW Task 5",
+#     "completed": true
+# }
