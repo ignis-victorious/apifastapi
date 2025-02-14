15015884e96d7aaa4f6f8f49124da49a656dad72
Emagnu <ignisvictorious@gmail.com>
2025-02-14 15:20:26 +0000

    added put and delete

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 #  ##   ###
 #  Import LIBRARIES
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from uuid import UUID, uuid4
 #  Import FILES
@@ -30,6 +30,41 @@ def create_task(task: Task) -> Task:
 @app.get("/tasks/", response_model=list[Task])
 def read_tasks() -> list[Task]:
     return tasks
+
+
+@app.get("/tasks/{task_id}", response_model=list[Task])
+def read_task(task_id: UUID) -> Task | HTTPException:
+    for task in tasks:
+        if task.id == task_id:
+            return task
+    raise HTTPException(status_code=404, detail="Task not found")
+
+
+@app.put("/tasks/{task_id}", response_model=list[Task])
+def update_task(task_id: UUID, task_update: Task) -> Task | HTTPException:
+    for idx, task in enumerate(tasks):
+        if task.id == task_id:
+            # task.title = task_update.title
+            # task.description = task_update.description
+            # task.completed = task_update.completed
+            # return task
+            # updated_task = task.copy(update=task_update.dict(exclude_unset=True))
+            updated_task = task.model_copy(
+                update=task_update.model_dump(exclude_unset=True)
+            )
+            tasks[idx] = updated_task
+            return updated_task
+
+    raise HTTPException(status_code=404, detail="Task not found")
+
+
+@app.delete("/tasks/{task_id}", response_model=list[Task])
+def delete_task(task_id: UUID) -> Task | HTTPException:
+    for idx, task in enumerate(tasks):
+        if task.id == task_id:
+            tasks.pop(idx)
+            return task
+    raise HTTPException(status_code=404, detail="Task not found")
 
 
 # @app.get("/")
